080320ffa224c78bd4952e6e5ebd2d33c9dbdcfe
Artem Konyshev <konyshev_arte@mail.ru>
2019-07-31 01:01:36 +0500

    Add relatives update; Refactor
    - Change db type from MongoClient to Database in make_app arguments
    - Add relatives update in citizen patch handler

--- index.py
+++ index.py
@@ -7,15 +7,16 @@ from typing import Tuple
 
 from flask import Flask, request
 from jsonschema.exceptions import ValidationError
-from pymongo import MongoClient, ReturnDocument
+from pymongo import MongoClient, ReturnDocument, UpdateMany
+from pymongo.database import Database
 from pymongo.errors import PyMongoError
-from pymongo.results import InsertOneResult
+from pymongo.results import InsertOneResult, BulkWriteResult
 from werkzeug.exceptions import BadRequest
 
 from data_validator import DataValidator
 
 
-def make_app(db: MongoClient, data_validator: DataValidator) -> Flask:
+def make_app(db: Database, data_validator: DataValidator) -> Flask:
     app = Flask(__name__)
 
     def make_error_response(message: str, status_code: int) -> Tuple[dict, int]:
@@ -55,12 +56,37 @@ def make_app(db: MongoClient, data_validator: DataValidator) -> Flask:
 
     @app.route('/imports/<int:import_id>/citizens/<int:citizen_id>', methods=['PATCH'])
     def citizen(import_id: int, citizen_id: int):
+
+        def make_update_relative_request(operation: str, relative_ids: list):
+            return UpdateMany({'import_id': import_id},
+                              {operation: {'citizens.$[element].relatives': citizen_id}},
+                              array_filters=[{'element.citizen_id': {'$in': relative_ids}}])
+
         if not request.is_json:
             return make_error_response('Content-Type must be application/json', 400)
 
         try:
             patch_data = request.get_json()
             data_validator.validate_citizen_patch(patch_data)
+            if 'relatives' in patch_data:
+                old_relatives_response: dict = db['imports'].find_one({'import_id': import_id},
+                                                                      {'citizens': {
+                                                                          '$elemMatch': {'citizen_id': citizen_id}}})
+                if old_relatives_response is None:
+                    return make_error_response('Import or citizen with specified id not found', 400)
+                old_relatives = set(old_relatives_response['citizens'][0]['relatives'])
+                new_relatives = set(patch_data['relatives'])
+                to_push = new_relatives - old_relatives
+                to_pull = old_relatives - new_relatives
+                db_requests = []
+                if len(to_push) > 0:
+                    db_requests.append(make_update_relative_request('$push', list(to_push)))
+                if len(to_pull) > 0:
+                    db_requests.append(make_update_relative_request('$pull', list(to_pull)))
+                if len(db_requests) > 0:
+                    bulk_response: BulkWriteResult = db['imports'].bulk_write(db_requests)
+                    if bulk_response.modified_count != len(db_requests):
+                        return make_error_response('Relative with specified id not found', 400)
 
             update_data = {
                 '$set': {f'citizens.$.{key}': val for key, val in patch_data.items()}
@@ -77,7 +103,6 @@ def make_app(db: MongoClient, data_validator: DataValidator) -> Flask:
                 projection=projection, return_document=ReturnDocument.AFTER)
             if db_response is None:
                 return make_error_response('Import or citizen with specified id not found', 400)
-
             return {'data': db_response['citizens'][0]}, 201
         except ValidationError as e:
             return make_error_response('Citizen patch is not valid: ' + str(e), 400)
